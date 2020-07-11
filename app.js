6be1ce64871f8ca78a7aaac78cdf0a8612caabea
gusgewehr <52550134+gusgewehr@users.noreply.github.com>
2020-07-11 14:54:31 -0300

    erro

--- app.js
+++ app.js
@@ -3,17 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+//var handlebars =  require('express-handlebars');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var newRouter = require('./routes/new');
-var mainRouter = require('./routes/main');
+var tableRouter = require('./routes/table');
 
 var app = express();
 
 // view engine setup
+//app.engine('handlebars', handlebars({defaultLayout: 'main'}))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+//app.set ('view engine', 'handlebars')
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -24,7 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/new', newRouter);
-app.use('/main', mainRouter);
+app.use('/table', tableRouter);
 
 
 // catch 404 and forward to error handler
